bdde8e696126f0b8e3703aec1fb2821b7f1be4e2
Ch0ckblain <rouraarturo@protonmail.com>
2022-12-09 22:12:14 +0100

    goerli testnet check

--- pages/index.js
+++ pages/index.js
@@ -12,35 +12,40 @@ export default function Home() {
     const chainString = chainId ? parseInt(chainId).toString() : "1337"
 
     const marketplaceAddress = networkMapping[chainString]["NFTMarket"][0]
-
+    console.log(chainString)
     const { loading, error, data: listedNfts } = useQuery(GET_ACTIVE_ITEMS)
 
     return (
         <div className="container mx-auto">
-            <h1 className="py-4 px-4 font-bold text-2xl">Recently Listed</h1>
+            <h1 className="py-4 px-4 font-bold text-2xl">
+                {chainString != 5 ? "Please connect to the Göerli testnet" : "Recently Listed NFTs"}
+            </h1>
             <div className="flex flex-wrap space-x-6">
                 {isWeb3Enabled ? (
-                    chainString == 4 ? (
+                    chainString == 5 ? (
                         loading || !listedNfts ? (
                             <div>loading...</div>
                         ) : (
-                            listedNfts.activeItems.map((nft) => {
-                                console.log(nft)
-                                const { price, nftAddress, tokenId, seller } = nft
-                                return (
-                                    <NFTBox
-                                        price={price}
-                                        nftAddress={nftAddress}
-                                        tokenId={tokenId}
-                                        marketplaceAddress={marketplaceAddress}
-                                        seller={seller}
-                                        key={`${nftAddress}${tokenId}`}
-                                    />
-                                )
-                            })
+                            <div>
+                                <div className="py-4 px-4">Click on the NFT to buy it! </div>
+                                {listedNfts.activeItems.map((nft) => {
+                                    console.log(nft)
+                                    const { price, nftAddress, tokenId, seller } = nft
+                                    return (
+                                        <NFTBox
+                                            price={price}
+                                            nftAddress={nftAddress}
+                                            tokenId={tokenId}
+                                            marketplaceAddress={marketplaceAddress}
+                                            seller={seller}
+                                            key={`${nftAddress}${tokenId}`}
+                                        />
+                                    )
+                                })}
+                            </div>
                         )
                     ) : (
-                        <div> Please connect to the Göerli testnet </div>
+                        <div className="py-4 px-4"> Thank you for your patience </div>
                     )
                 ) : (
                     <div> Web3 currently not enabled, please connect your wallet</div>
